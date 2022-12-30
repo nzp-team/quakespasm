a1610a77f21a0ae953723c5b512aa6a6385725ce
Steam Deck User <motolegacy.git@gmail.com>
2022-12-30 12:37:03 -0500

    Use STBImage for TGA instead of internal.

--- source/image.c
+++ source/image.c
@@ -106,7 +106,7 @@ byte *Image_LoadImage (const char *name, int *width, int *height)
 	q_snprintf (loadfilename, sizeof(loadfilename), "%s.tga", name);
 	COM_FOpenFile (loadfilename, &f, NULL);
 	if (f) {
-		return Image_LoadTGA (f, width, height);
+		return Image_LoadSTBI (f, width, height);
 	}
 
 	q_snprintf (loadfilename, sizeof(loadfilename), "%s.png", name);
