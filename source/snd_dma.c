466a375729f9251a685673797b3b30e46082604f
cypress <motolegacy.git@gmail.com>
2023-11-30 12:30:48 -0500

    NX/VITA: Don't spatialize sounds with no attenuation

--- source/snd_dma.c
+++ source/snd_dma.c
@@ -398,8 +398,9 @@ void SND_Spatialize (channel_t *ch)
 	vec_t	lscale, rscale, scale;
 	vec3_t	source_vec;
 
-// anything coming from the view entity will always be full volume
-	if (ch->entnum == cl.viewentity)
+	// anything coming from the view entity will always be full volume
+	// cypress -- added full volume for no attenuation.
+	if (ch->entnum == cl.viewentity || ch->dist_mult == 0)
 	{
 		ch->leftvol = ch->master_vol;
 		ch->rightvol = ch->master_vol;
